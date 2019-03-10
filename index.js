da589df052cc581a8ecf7ba74340acf6c95926ec
unknown <chris.a.desilva@gmail.com>
2019-03-10 11:26:43 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send({ hi: 'there' });
+});
+
+//allows Heroku to determine port or use 5000 when in development environment
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
